90a0f851795738f3e930ae9cc2e9221a27be0ab0
Thomas von Rosenberg <tvr@omikron.net>
2021-02-24 15:10:22 +0100

    Add cli

--- main.go
+++ main.go
@@ -10,35 +10,72 @@ import (
 	"github.com/FACT-Finder/perfably/server"
 	"github.com/go-redis/redis/v8"
 	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
 	logger.Init(zerolog.ErrorLevel)
-	if len(os.Args) != 2 {
-		log.Fatal().Msg("Requires on config parameter")
-		return
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "redis-address",
+				Usage:   "address of redis server",
+				EnvVars: []string{"PERFABLY_REDIS_ADDRESS"},
+				Value:   ":6379",
+			},
+			&cli.StringFlag{
+				Name:    "redis-password",
+				EnvVars: []string{"PERFABLY_REDIS_PASSWORD"},
+				Usage:   "password of redis server",
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name:  "serve",
+				Usage: "start the web service",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "config",
+						Aliases:  []string{"c"},
+						EnvVars:  []string{"PERFABLY_CONFIG"},
+						Usage:    "load configuration from `FILE`",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:    "address",
+						Usage:   "perfably listen address",
+						EnvVars: []string{"PERFABLY_ADDRESS"},
+						Value:   ":8000",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					cfg, err := config.New(c.String("config"))
+					if err != nil {
+						return err
+					}
+
+					redisAddr := c.String("redis-address")
+					client := redis.NewClient(&redis.Options{
+						Addr:     redisAddr,
+						Password: c.String("redis-password"),
+					})
+					r := router.New(cfg, client)
+
+					listenAddr := c.String("address")
+					fmt.Println("Listening on", listenAddr)
+					err = server.Start(r, listenAddr)
+					if err != nil {
+						return err
+					}
+					return nil
+				},
+			},
+		},
 	}
 
-	cfg, err := config.New(os.Args[1])
+	err := app.Run(os.Args)
 	if err != nil {
-		log.Fatal().Err(err).Msg("Could not read config")
-		return
-	}
-
-	addr := os.Getenv("PERFABLY_ADDRESS")
-	if addr == "" {
-		addr = ":8000"
-	}
-
-	client := redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("PERFABLY_REDIS_ADDRESS"),
-		Password: os.Getenv("PERFABLY_REDIS_PASSWORD"),
-	})
-	r := router.New(cfg, client)
-	fmt.Println("Listening on", addr)
-	err = server.Start(r, addr)
-	if err != nil {
-		log.Fatal().Err(err).Msg("could not start server")
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
