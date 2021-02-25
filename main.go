5876ae376ed8045e75897a328f8e2a2f2a3debe1
Thomas von Rosenberg <tvr@omikron.net>
2021-02-25 11:38:18 +0100

    Add token cli command

--- main.go
+++ main.go
@@ -4,11 +4,8 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/FACT-Finder/perfably/config"
+	"github.com/FACT-Finder/perfably/cmd"
 	"github.com/FACT-Finder/perfably/logger"
-	"github.com/FACT-Finder/perfably/router"
-	"github.com/FACT-Finder/perfably/server"
-	"github.com/go-redis/redis/v8"
 	"github.com/rs/zerolog"
 	"github.com/urfave/cli/v2"
 )
@@ -30,46 +27,8 @@ func main() {
 			},
 		},
 		Commands: []*cli.Command{
-			{
-				Name:  "serve",
-				Usage: "start the web service",
-				Flags: []cli.Flag{
-					&cli.StringFlag{
-						Name:     "config",
-						Aliases:  []string{"c"},
-						EnvVars:  []string{"PERFABLY_CONFIG"},
-						Usage:    "load configuration from `FILE`",
-						Required: true,
-					},
-					&cli.StringFlag{
-						Name:    "address",
-						Usage:   "perfably listen address",
-						EnvVars: []string{"PERFABLY_ADDRESS"},
-						Value:   ":8000",
-					},
-				},
-				Action: func(c *cli.Context) error {
-					cfg, err := config.New(c.String("config"))
-					if err != nil {
-						return err
-					}
-
-					redisAddr := c.String("redis-address")
-					client := redis.NewClient(&redis.Options{
-						Addr:     redisAddr,
-						Password: c.String("redis-password"),
-					})
-					r := router.New(cfg, client)
-
-					listenAddr := c.String("address")
-					fmt.Println("Listening on", listenAddr)
-					err = server.Start(r, listenAddr)
-					if err != nil {
-						return err
-					}
-					return nil
-				},
-			},
+			cmd.Serve(),
+			cmd.Token(),
 		},
 	}
 
